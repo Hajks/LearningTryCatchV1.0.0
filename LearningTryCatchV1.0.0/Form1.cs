bac1bca0a4c0a217bedb5ca39fae1ac52f2a6619
Hajk <tunczorx@gmail.com>
2018-07-25 11:28:16 +0200

    Simple example of try and catch use.

--- LearningTryCatchV1.0.0/Form1.cs
+++ LearningTryCatchV1.0.0/Form1.cs
@@ -12,9 +12,51 @@ namespace LearningTryCatchV1._0._0
 {
     public partial class Form1 : Form
     {
+        HoneyDeliverySystem delivery = new HoneyDeliverySystem();
+
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                delivery.FeedHoneyToEggs();
+            }
+            catch (OutOfHoneyException ex)
+            {
+                MessageBox.Show(ex.Message, "Ostrzeżnie: brak miodu, uzupełniam zasoby z rezerw.");
+                delivery.HoneyLevel = 0;
+                MessageBox.Show("Miód uzupełniony.");
+
+            }
+            finally
+            {
+
+                MessageBox.Show("Ul aktualnie pracuje.");
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                delivery.FeedHoneyToEggs();
+            }
+            catch (OutOfHoneyException ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ostrzeżnie: brak miodu, uzupełniam zasoby z rezerw.");
+                delivery.HoneyLevel = 1;
+                MessageBox.Show("Miód uzupełniony.");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message,"Złapałem");
+            }
+            finally
+            {
+                MessageBox.Show("Ul aktualnie pracuje.");
+                throw new Exception("Rzucanie nowego wyjątku np. Błąd nr : 404");
+            }
         }
     }
 }
---
+++ LearningTryCatchV1.0.0/HoneyDeliverySystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningTryCatchV1._0._0
+{
+    class HoneyDeliverySystem
+    {
+        public int HoneyLevel = 0;
+        public void FeedHoneyToEggs()
+        {
+            if (HoneyLevel == 0)
+            {
+                throw new OutOfHoneyException("W ulu brakło miodu");
+            }
+            else
+            {
+                MessageBox.Show("Nie brakuje nam miodu!");
+            }
+        }
+        
+    }
+}
